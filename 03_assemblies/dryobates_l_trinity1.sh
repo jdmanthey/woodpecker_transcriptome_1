e5f3afafdb56a9ae9657548c0bc122b41e26c949
Joe Manthey <jdmanthey@gmail.com>
2019-04-18 13:42:57 -0500

    Update dryobates_l_trinity1.sh

--- 03_assemblies/dryobates_l_trinity1.sh
+++ 03_assemblies/dryobates_l_trinity1.sh
@@ -11,7 +11,7 @@
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=8G
 
- Trinity --seqType fq --max_memory 192G \ 
+ Trinity --seqType fq --max_memory 192G \
  --left /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1271_R1.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1273_R1.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1278_R1.fastq.gz \
  --right /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1271_R2.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1273_R2.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_L_LHD1278_R2.fastq.gz \
  --CPU 24 --output dryobates_l_trinity1
