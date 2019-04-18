8d0d52f7742113b51b2534b9bac40245d520c9d9
Joe Manthey <jdmanthey@gmail.com>
2019-04-18 13:41:03 -0500

    Update pileatus_g_trinity1.sh

--- 03_assemblies/pileatus_g_trinity1.sh
+++ 03_assemblies/pileatus_g_trinity1.sh
@@ -11,7 +11,7 @@
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=8G
 
- Trinity --seqType fq --max_memory 192G \ 
+ Trinity --seqType fq --max_memory 192G \
  --left /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pileatus_G_LHD1282_R1.fastq.gz \
  --right /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pileatus_G_LHD1282_R2.fastq.gz \
  --CPU 24 --output pileatus_g_trinity1
