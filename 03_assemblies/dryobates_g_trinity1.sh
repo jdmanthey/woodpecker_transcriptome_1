65548427e9adf7e974bcef550bfc36015201a62e
Joe Manthey <jdmanthey@gmail.com>
2019-04-18 13:31:08 -0500

    Create dryobates_g_trinity1.sh

---
+++ 03_assemblies/dryobates_g_trinity1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N dryobates_g_trinity1
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -pe sm 24
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+ Trinity --seqType fq --max_memory 192G \ 
+ --left /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1271_R1.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1273_R1.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1278_R1.fastq.gz \
+ --right /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1271_R2.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1273_R2.fastq.gz,/lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1278_R2.fastq.gz \
+ --CPU 24 --output dryobates_g_trinity1
+ 
