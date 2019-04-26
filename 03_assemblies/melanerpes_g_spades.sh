0c4fe2e96f9a9f10c21ff19fbdc6f8f25fbaa6c1
Joe Manthey <jdmanthey@gmail.com>
2019-04-26 15:51:17 -0500

    Update melanerpes_g_spades.sh

--- 03_assemblies/melanerpes_g_spades.sh
+++ 03_assemblies/melanerpes_g_spades.sh
@@ -17,4 +17,4 @@ spades.py --rna \
 --pe2-2 /lustre/scratch/sabaty/melanerpes-project/transcriptomes/01_filtered_data/M_carolinus_G_LHD1274_R2.fastq.gz \
 --pe3-1 /lustre/scratch/sabaty/melanerpes-project/transcriptomes/01_filtered_data/M_carolinus_G_LHD1275_R1.fastq.gz \
 --pe3-2 /lustre/scratch/sabaty/melanerpes-project/transcriptomes/01_filtered_data/M_carolinus_G_LHD1275_R2.fastq.gz \
--o /lustre/scratch/jmanthey/11_transcriptomes/02_assemblies/melanerpes_g_spades --phred-offset 33 -t 24 -m 500
+-o /lustre/scratch/jmanthey/11_transcriptomes/02_assemblies/melanerpes_g_spades --phred-offset 33 -t 16 -m 500
