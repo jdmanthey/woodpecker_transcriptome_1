e08fd64f52e457a7e385bd63cce9e4d05b379f4c
Joe Manthey <jdmanthey@gmail.com>
2019-04-26 16:14:28 -0500

    Update dryobates_g_spades.sh

--- 03_assemblies/dryobates_g_spades.sh
+++ 03_assemblies/dryobates_g_spades.sh
@@ -5,11 +5,10 @@
 #$ -N dryobates_g_spades
 #$ -o $JOB_NAME.o$JOB_ID
 #$ -e $JOB_NAME.e$JOB_ID
-#$ -q omni
+#$ -q Yoda
 #$ -pe sm 24
-#$ -P quanah
-#$ -l h_rt=48:00:00
-#$ -l h_vmem=8G
+#$ -P communitycluster
+
 
 spades.py --rna --pe1-1 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1271_R1.fastq.gz \
 --pe1-2 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1271_R2.fastq.gz \
@@ -17,5 +16,5 @@ spades.py --rna --pe1-1 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_d
 --pe2-2 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1273_R2.fastq.gz \
 --pe3-1 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1278_R1.fastq.gz \
 --pe3-2 /lustre/scratch/jmanthey/11_transcriptomes/01_filtered_data/D_pubescens_G_LHD1278_R2.fastq.gz \
--o /lustre/scratch/jmanthey/11_transcriptomes/dryobates_g_spades --phred-offset 33 -t 24 -m 192
+-o /lustre/scratch/jmanthey/11_transcriptomes/dryobates_g_spades --phred-offset 33 -t 24 -m 500
 
